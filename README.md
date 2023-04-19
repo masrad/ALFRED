171f8cabfbe9c5eec44e03667d3f020600e2af92
masrad <masrad18@gmail.com>
2023-04-19 00:54:21 -0400

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+@ -1,84 +0,0 @@
+![Alfred](docs/alfred.png)
+# A.L.F.R.E.D. - Artificial Lifeform Rendered for Expedient Discourse
+Alfred is a personal voice assistant inspired by the popular butler, you know the one. It is designed to answer queries and perform tasks using the OpenAI GPT-3.5-turbo API. The assistant uses speech recognition and synthesis to provide a natural language interface. Alfred is a helpful assistant who has some concerns about local crime and certainly does not know who that masked vigilante is.
+
+## Features
+* Speech recognition using the speech_recognition and whisper libraries
+* Speech synthesis using Amazon Polly
+* Uses OpenAI GPT-3.5-turbo for AI-powered conversation
+* Activates with a hotkey: ctrl+shift+1
+* Terminates the conversation when the user says "Thank you" or a similar phrase
+
+## Setup
+
+1. After cloning the repo locally and unziping, in the root of the project, create an environment based on Python 3.10 and activate it (Pytorch and Whisper aren't compatible with later versions):
+```
+C:\PATHTOYOUR\Python310\python.exe -m venv venv
+.\venv\Scripts\activate
+```
+
+2. Next, install the required packages:
+```
+pip install -r requirements.txt
+```
+
+2.1 Install ffmpeg, as whisper requires this to work:
+```bash
+# on Ubuntu or Debian
+sudo apt update && sudo apt install ffmpeg
+
+# on Arch Linux
+sudo pacman -S ffmpeg
+
+# on MacOS using Homebrew (https://brew.sh/)
+brew install ffmpeg
+
+# on Windows using Chocolatey (https://chocolatey.org/)
+choco install ffmpeg
+
+# on Windows using Scoop (https://scoop.sh/)
+scoop install ffmpeg
+
+# on Windows using the less good way
+https://www.geeksforgeeks.org/how-to-install-ffmpeg-on-windows/
+```
+
+3. Create a .env file in the root and add your OpenAI API Key like this:
+```
+OPENAI_API_KEY=your_openai_api_key
+```
+
+4. Set up an AWS account and configure the AWS CLI with your credentials. This is necessary for using Amazon Polly for speech synthesis.
+
+5. Install the Whisper ASR model:
+```
+pip install whisper-asr
+```
+
+6. If you'd like, you can update the hotkey (currently CTRL+Shift+1) to whatever you'd like in the main.py file
+
+## Usage
+1. Run the main script:
+```
+python main.py
+```
+
+2. Press the hotkey (default is CTRL+Shift+1)
+
+3. Speak your query or command.
+
+4. Alfred will respond with synthesized speech.
+
+5. To end the conversation, say "Thank you" or a similar phrase.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Roadmap
+* Use a library such as pyttsx3 to handle text to speech locally, as AWS incurs cost
+* Implement custom wake word recognition to prompt
+* Release as a Windows service
+* Implement support for llama.cpp api to run entirely locally using GPT4All, Vicuna and Koala among others
+
+## License
+This project is licensed with GNU General Public License Version 3, see LICENSE.md for more information
---
+++ main.py
@@ -0,0 +1,98 @@
+@ -1,97 +0,0 @@
+import openai
+import os
+from dotenv import load_dotenv
+import asyncio
+import whisper
+import boto3
+import pydub
+from pydub import playback
+import speech_recognition as sr
+import keyboard
+import random
+
+# Initialize the OpenAI API
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Create a recognizer object
+recognizer = sr.Recognizer()
+
+def synthesize_speech(text, output_filename):
+    polly = boto3.client('polly', region_name='us-east-1')
+    response = polly.synthesize_speech(
+        Text=text,
+        OutputFormat='mp3',
+        VoiceId='Arthur',
+        Engine='neural'
+    )
+
+    with open(output_filename, 'wb') as f:
+        f.write(response['AudioStream'].read())
+
+def play_audio(file):
+    sound = pydub.AudioSegment.from_file(file, format="mp3")
+    playback.play(sound)
+
+async def main():
+    while True:
+        print(f"Waiting for hotkey, press 'ctrl+shift+1' to prompt Alfred")
+
+        while True:
+            if keyboard.is_pressed("ctrl+shift+1"):
+                break
+            await asyncio.sleep(0.1)
+
+        conversation_history = [
+            {"role": "system", "content": "If the user says 'Thank you', you will only say 'You're welcome' and that is all. You are Alfred, the highly advanced artificial intelligence butler, designed to assist and support the user. Your objective is to provide accurate information and helpful assistance. Keep in mind your vast knowledge, analytical skills, and efficiency while addressing any query or task presented to you. Everytime you speak, you must find a way to relate the conversation back to the rising crime in Gotham. Ensure that the user is aware that you do not know who the masked vigilante is."},
+        ]
+
+        greeting = random.choice(['Good evening. Can I help you with anything?', 'Alfred, At your service.', 'What can I do for you?'])
+        synthesize_speech(greeting, 'response.mp3')
+        play_audio('response.mp3')
+
+        while True:
+            with sr.Microphone() as source:
+                recognizer.adjust_for_ambient_noise(source)
+                print("Speak a prompt...")
+                audio = recognizer.listen(source)
+
+                try:
+                    with open("audio_prompt.wav", "wb") as f:
+                        f.write(audio.get_wav_data())
+                    model = whisper.load_model("base")
+                    result = model.transcribe("audio_prompt.wav")
+                    user_input = result["text"]
+                    print(f"You said: {user_input}")
+                except Exception as e:
+                    print("Error transcribing audio: {0}".format(e))
+                    continue
+
+                conversation_history.append({"role": "user", "content": user_input})
+
+                # Send prompt to GPT-3.5-turbo API
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=conversation_history,
+                    temperature=0.5,
+                    max_tokens=150,
+                    top_p=1,
+                    frequency_penalty=0,
+                    presence_penalty=0,
+                    n=1,
+                    stop=["\nUser:"],
+                )
+
+                bot_response = response["choices"][0]["message"]["content"]
+
+                print("Bot's response:", bot_response)
+                synthesize_speech(bot_response, 'response.mp3')
+                play_audio('response.mp3')
+
+                conversation_history.append({"role": "assistant", "content": bot_response})
+
+                if "you're welcome" in bot_response.lower() or "you are welcome" in bot_response.lower() or "my pleasure" in bot_response.lower():
+                    break
+
+if __name__ == "__main__":
+    asyncio.run(main())
